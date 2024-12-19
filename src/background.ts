e4c5f77396cc4d66a6e4aac79d65d33a6ad2c6d8
Vinicius Benedito <beneditoloura@gmail.com>
2024-12-19 06:59:42 -0300

    refactor: remove unnecessary code

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,4 @@
 chrome.runtime.onMessage.addListener((message, _, sendResponse) => {
-  console.log("Message from popup:", message);
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     if (tabs[0]?.id) {
       chrome.scripting.insertCSS({
@@ -8,7 +7,6 @@ chrome.runtime.onMessage.addListener((message, _, sendResponse) => {
       });
 
       chrome.tabs.sendMessage(tabs[0].id, message, (response) => {
-        console.log("Response from content script:", response);
         sendResponse(response);
       });
     }
